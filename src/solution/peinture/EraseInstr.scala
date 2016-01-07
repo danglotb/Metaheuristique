021dee58ab773028af17107c1d62ef404ed1efc4
bdanglot <bdanglot@gmail.com>
2016-01-07 18:14:45 +0100

    done Erase

--- src/solution/peinture/EraseInstr.scala
+++ src/solution/peinture/EraseInstr.scala
@@ -2,9 +2,7 @@ package solution.peinture
 
 class EraseInstr(R: Int, C: Int) extends AbstractInstr(R,C){
 
-  def apply(l : List[List[Boolean]]): List[List[Boolean]] = {
-    List.fill(1, 1)(false)
-  }
+  def apply(l : List[List[Boolean]]): List[List[Boolean]] = l.updated(R,l(R).updated(C, false))
 
   override def toString = "ERASECELL " + r + " " + c + "\n"
   
